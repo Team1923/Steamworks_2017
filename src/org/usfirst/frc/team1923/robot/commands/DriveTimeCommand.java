4f4715e07af9deb4b0a30dba10af23dadb37d879
Sahit Penmatcha <sahilitrix123@gmail.com>
2017-02-10 18:53:57 -0500

    DriveTime Command added

--- src/org/usfirst/frc/team1923/robot/commands/DriveTimeCommand.java
+++ src/org/usfirst/frc/team1923/robot/commands/DriveTimeCommand.java
@@ -1,5 +1,38 @@
 package org.usfirst.frc.team1923.robot.commands;
 
-public class DriveTimeCommand {
+import org.usfirst.frc.team1923.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DriveTimeCommand extends Command{
+
+	private double speed;
+	
+	public DriveTimeCommand(double speed, double timeOut){
+		requires(Robot.driveSubSystem);
+		setTimeout(timeOut);
+		this.speed = speed;
+	}
+
+	protected void initialize() {
+		
+	}
+
+	protected void execute() {
+		Robot.driveSubSystem.set(speed, speed);
+	}
+
+	protected boolean isFinished() {
+		return isTimedOut();
+	}
+
+	protected void end() {
+		Robot.driveSubSystem.stop();
+	}
+
+	protected void interrupted() {
+		end();
+	}
+
 
 }
