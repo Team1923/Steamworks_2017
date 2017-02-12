01ab8017c57d60bb1c5887f6cacc1d6300c470b7
Tim Magoun <timmagoun@gmail.com>
2017-02-12 13:10:01 -0500

    Changed to operator only because ps4 doesn't have standard rumble

--- src/org/usfirst/frc/team1923/robot/commands/ControllerRumbleCommand.java
+++ src/org/usfirst/frc/team1923/robot/commands/ControllerRumbleCommand.java
@@ -1,7 +1,9 @@
 package org.usfirst.frc.team1923.robot.commands;
 
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
-import edu.wpi.first.wpilibj.Joystick;
+
+import org.usfirst.frc.team1923.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.TimedCommand;
 
 /**
@@ -9,42 +11,37 @@ import edu.wpi.first.wpilibj.command.TimedCommand;
  */
 public class ControllerRumbleCommand extends TimedCommand {
 
-	private Joystick controller;
 	private double power;
 
 	/**
-	 * Command to rumble the controller of the driver and operator for a certain
+	 * Command to rumble the controller of the operator for a certain
 	 * amount of time. Default power of 50%
 	 * 
 	 * @param timeout
 	 *            time to rumble
-	 * @param controller
-	 *            the controller to rumble (declared in OI)
 	 */
-	public ControllerRumbleCommand(double timeout, Joystick controller) {
+	public ControllerRumbleCommand(double timeout) {
 		super(timeout);
-		this.controller = controller;
 		this.power = 0.5;
 	}
 
 	/**
-	 *Command to rumble the controller of the driver and operator for a certain
+	 *Command to rumble the controller of the operator for a certain
 	 * amount of time.
 	 * 
 	 * @param timeout time to rumble
 	 * @param controller controller to rumble
 	 * @param power 0-1 intensity of rumble
 	 */
-	public ControllerRumbleCommand(double timeout, Joystick controller, double power) {
+	public ControllerRumbleCommand(double timeout, double power) {
 		super(timeout);
-		this.controller = controller;
 		this.power = power;
 	}
 
 	// Called just before this Command runs the first time
 	protected void initialize() {
-		controller.setRumble(RumbleType.kRightRumble, power);
-		controller.setRumble(RumbleType.kLeftRumble, power);
+		Robot.oi.op.setRumble(RumbleType.kRightRumble, power);
+		Robot.oi.op.setRumble(RumbleType.kLeftRumble, power);
 	}
 
 	// Called repeatedly when this Command is scheduled to run
@@ -53,8 +50,8 @@ public class ControllerRumbleCommand extends TimedCommand {
 
 	// Called once after timeout
 	protected void end() {
-		controller.setRumble(RumbleType.kLeftRumble, 0);
-		controller.setRumble(RumbleType.kRightRumble, 0);
+		Robot.oi.op.setRumble(RumbleType.kLeftRumble, 0);
+		Robot.oi.op.setRumble(RumbleType.kRightRumble, 0);
 	}
 
 	// Called when another command which requires one or more of the same
