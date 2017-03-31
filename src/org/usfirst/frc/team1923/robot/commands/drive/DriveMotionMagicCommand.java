091ded1ebd527184bc69df386a87c7f08fa0be84
James <admin@lyph.io>
2017-03-30 23:05:13 -0400

    Add ending point for motion magic

--- src/org/usfirst/frc/team1923/robot/commands/drive/DriveMotionMagicCommand.java
+++ src/org/usfirst/frc/team1923/robot/commands/drive/DriveMotionMagicCommand.java
@@ -15,11 +15,12 @@ public class DriveMotionMagicCommand extends Command {
 
     private double dist;
     private double target;
+    private int targetEncoderTick;
 
     public DriveMotionMagicCommand(double dist) {
         this.dist = dist;
         requires(Robot.driveSubSys);
-        SmartDashboard.putNumber("Dist to run", 0);
+        SmartDashboard.putNumber("Dist to run", dist);
     }
 
     // Called just before this Command runs the first time
@@ -29,15 +30,14 @@ public class DriveMotionMagicCommand extends Command {
         Robot.driveSubSys.configMM();
         Robot.driveSubSys.resetPosition();
 
-        System.out.println("Starting sleep");
         try {
             Thread.sleep(100);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println("Ending sleep");
 
         target = DrivetrainSubsystem.distanceToRotation(this.dist);
+        this.targetEncoderTick = (int) (target * 4000);
         System.out.println(target);
         Robot.driveSubSys.drive(target, target, TalonControlMode.MotionMagic);
     }
@@ -45,7 +45,9 @@ public class DriveMotionMagicCommand extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
-        System.out.println("Left Error: " + Robot.driveSubSys.getLeftError() + ", Right Error: " + Robot.driveSubSys.getRightError());
+        System.out.println("Left Error: " + Robot.driveSubSys.getLeftError() + ", Right Error: " + Robot.driveSubSys.getRightError() + ", Tick: "
+                + this.targetEncoderTick + ", Left: " + Robot.driveSubSys.getLeftEncPosition() + ", Right: "
+                + Robot.driveSubSys.getRightEncPosition());
 
         Robot.driveSubSys.drive(target, target, TalonControlMode.MotionMagic);
     }
@@ -53,7 +55,8 @@ public class DriveMotionMagicCommand extends Command {
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-        return false;
+        return (Math.abs(Robot.driveSubSys.getLeftEncPosition() - this.targetEncoderTick) < 300
+                && Math.abs(Robot.driveSubSys.getRightEncPosition() - this.targetEncoderTick) < 300);
     }
 
     // Called once after isFinished returns true
